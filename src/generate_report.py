5f5d9799292300b407006e8e01f09fc3afc8f505
Lauro Moura <lauromoura@gmail.com>
2016-05-21 00:00:24 -0300

    Refactor standings generator and output the GC placement.
    Also start making pylint happier

--- src/generate_report.py
+++ src/generate_report.py
@@ -3,20 +3,13 @@ import csv
 import glob
 import json
 
-longest_stage = 0
-
-
-# FIXME Implement this.
-# Color table for team:
-def color_for_team(name):
-    pass
 
 def load_stage_file(data, filename):
-    global longest_stage
     print "loading", filename
-    longest_stage += 1
     with open(filename) as handle:
-        current_time = -1
+        # We store it outside the loop for riders with the same
+        # time as a previous rider.
+        current_time = ""
         for line in handle:
             #print line
             tokens = line.split()
@@ -25,6 +18,7 @@ def load_stage_file(data, filename):
             country_index = tokens.index(country)
             name = tokens[0:country_index]
             team = tokens[country_index+1:]
+
             if team[-1].find(':') != -1:
                 current_time = team.pop()
             if int(pos) == 1:
@@ -32,7 +26,7 @@ def load_stage_file(data, filename):
 
             name = " ".join(name)
             team = " ".join(team)
-            time = map(int, current_time.split(":"))
+            time = [int(x) for x in current_time.split(":")]
             time = 3600*time[0] + 60*time[1] + time[2]
 
             if name not in data:
@@ -40,7 +34,10 @@ def load_stage_file(data, filename):
             else:
                 data[name]["time"].append(time)
 
-
+def rider_key(x):
+    """Generates a value for rider sorting."""
+    #It is equal to the number of stagest ridden minus the last gap.
+    return 1000000*len(x[-1]) - x[-1][-1]
 
 def main(argv=None):
 
@@ -59,48 +56,19 @@ def main(argv=None):
     for filename in files:
         load_stage_file(data, filename)
 
-
-    # FIXME - Kinda ugly and could be a method.
-    # FIXME - Also for partial giro results seems to be missing Nibali (leader) times.
     ordered_data = [] # [(cyclist, times)] # from last to next
 
     for current_name in data:
-        # print "Testing ", current_name, " list size:", len(ordered_data)
         current_times = data[current_name]["time"]
         current_team = data[current_name]["team"]
         current_country = data[current_name]["country"]
         current_stages = len(current_times)
 
-        if not ordered_data:
-            ordered_data.append((current_name, current_team, current_country, current_times))
-            continue
+        ordered_data.append((current_name, current_team, current_country, current_times))
 
-        for name_in, team_in, country_in, times_in in ordered_data:
-            if len(times_in) < current_stages:
-                # print current_name, " has ridden ore stages than ", name_in
-                continue # Current rider has ridden more stages. Keep going.
-            elif len(times_in) == current_stages:
-                # check if current rider is behind in the classification
-                if times_in[-1] < current_times[-1]:
-                    index = ordered_data.index((name_in, team_in, country_in, times_in))
-                    ordered_data.insert(index, (current_name, current_team, current_country, current_times))
-                    break
-                else:
-                    continue
-            else: #Current rider has ridden less stages and must be inserted
-                index = ordered_data.index((name_in, team_in, country_in, times_in))
-                ordered_data.insert(index, (current_name, current_team, current_country, current_times))
-                break
-        else:
-            ordered_data.append((current_name, current_team, current_country, current_times))
+    ordered_data.sort(key=rider_key)
 
-    #for i in ordered_data:
-    #    print i[0]
-    #print "sorted"
-    #for i in sorted([i[0] for i in ordered_data]):
-    #    print i
-    print len(ordered_data)
-
+    print "Total riders: ", len(ordered_data)
 
     # FIXME Could be a method.
     json_data = {}
@@ -118,11 +86,12 @@ def main(argv=None):
         cyclist_data["team"] = team
         cyclist_data["country"] = country
         cyclist_data["time"] = []
+        cyclist_data["pos"] = i
 
         if team not in teams:
             teams.append(team)
 
-        for stage, value in enumerate(times):
+        for value in times:
             cyclist_data["time"].append(value)
 
         json_data[name] = cyclist_data
@@ -141,16 +110,6 @@ def main(argv=None):
 
     # with open("data/teamsraw.json", "w") as handle:
     #     handle.write(json.dumps(teams_dict))
-
-    # with open("output.csv", "wb") as handle:
-    #     csvwriter = csv.writer(handle)
-    #     headers = tuple(map((lambda x: "stage %d" % x), range(longest_stage)))
-    #     csvwriter.writerow(("Name",)+ headers)
-    #     for i, v in enumerate(sorted(data.keys())):
-    #         print i, v
-    #         print "%s,%s" % (v, ",".join(map(str, data[v]["time"])))
-    #         csvwriter.writerow((v,) + tuple(data[v]["time"]))
-
 
 if __name__ == '__main__':
     main()
